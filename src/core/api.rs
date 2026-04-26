fa8437cd60b5009fe1e3fa229c8cfa651e021150
Carlos Fabian Corrales Zapata <166270892+AppBlitz@users.noreply.github.com>
2026-04-25 20:18:00 -0500

    modification client

--- src/core/api.rs
+++ src/core/api.rs
@@ -1,17 +1,22 @@
+use reqwest::{
+    Result,
+    blocking::{self, Response},
+    header::USER_AGENT,
+};
+
 use crate::model::github_events::Events;
-use reqwest::{Result, blocking};
 
-static CLIENTS: std::sync::LazyLock<blocking::Client> =
-    std::sync::LazyLock::new(|| match blocking::Client::builder().build() {
-        Ok(value) => {
-            return value;
-        }
-        Err(err) => panic!("{err:?}"),
-    });
-
-pub fn get_user(name_user_github: String) -> Result<Vec<Events>> {
+fn response_client(client: &blocking::Client, name_user_github: String) -> Result<Response> {
     let uri: String = format!("https://api.github.com/users/{name_user_github}/events");
-    let body = CLIENTS.get(uri).send()?;
-    let res: Vec<Events> = body.json()?;
-    Ok(res)
+    let response = client
+        .get(uri)
+        .header(USER_AGENT, "github-user-activity")
+        .send()?;
+    Ok(response)
+}
+
+pub fn get_data(client: &blocking::Client, name_user_github: String) -> Result<Vec<Events>> {
+    let response = response_client(client, name_user_github)?;
+    let body: Vec<Events> = response.json()?;
+    Ok(body)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,11 @@
 use clap::Parser;
-use commands::{cli::CommandUsername, core::api::get_user, infra::functions::response_pdf};
+use commands::{cli::CommandUsername, core::api::get_data, infra::functions::response_pdf};
+use reqwest::blocking;
 
 fn main() {
     let args = CommandUsername::parse();
-    if let Ok(value) = get_user(args.user_name_github) {
+    let client: blocking::Client = reqwest::blocking::Client::new();
+    if let Ok(value) = get_data(&client, args.user_name_github) {
         response_pdf(&args.name_repository, &value);
     }
 }
